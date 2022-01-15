37f1cda9274da9c1ebab74eef3049e53988169e7
Brian Kung <github@briankung.xyz>
2022-01-15 15:19:50 -0600

    Add devise config modifications

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :investors
   resources :investors
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "investors#index"
 end
